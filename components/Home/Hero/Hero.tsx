e9a9ac85b2625d75b3229608618bba63286cf998
Wilson Frederique <wilsonfrederique3@gmail.com>
2025-07-20 16:50:09 +0200

    Hero bg video

---
+++ components/Home/Hero/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='relative w-full h-[120vh] sm:h-[100vh]'>
+        {/* OverLay  */}
+        <div className="absolute top-0 left-0 w-full h-full bg-gray-800 opacity-70"></div>
+        <video src="/images/bghero.mp4" autoPlay muted loop preload='metadata' className='w-full h-full object-cover' />
+    </div>
+  )
+}
+
+export default Hero
