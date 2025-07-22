6cca757d5e1e70dcc886ce0b6700537e88cba497
Wilson Frederique <wilsonfrederique3@gmail.com>
2025-07-22 15:27:55 +0200

    Frontend

--- components/Home/Newslettre/Newslettre.tsx
+++ components/Home/Newslettre/Newslettre.tsx
@@ -4,7 +4,7 @@ import { BsEnvelopePaper } from 'react-icons/bs'
 const Newslettre = () => {
   return (
     <div className='bg-black pt-16 pb-16 flex items-center justify-center w-full flex-col'>
-        <BsEnvelopePaper className='w-16 h-16 mt-20 text-white' />
+        <BsEnvelopePaper data-aos="zoom-in" className='w-16 h-16 mt-20 text-white' />
 
         <h1 className='text-lg sm:text-xl md:text-2xl text-white font-semibold mt-10 tracking-widest'>
             Your Travel Journey Starts Here
